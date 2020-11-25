4fe8b39399b9c4f07e7039815420ff065764e80d
Kye Davey <kye.j.davey@gmail.com>
2020-11-25 23:01:29 +1100

    style FaceRecognition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center'>
-      <img src={imageUrl} alt=""/>
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img src={imageUrl} width='500px' height='auto' alt=""/>
+      </div>
     </div>
   );
 }
