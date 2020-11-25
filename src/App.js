608d51c9ad6af10df5da0831194c7181f39ccf65
Kye Davey <kye.j.davey@gmail.com>
2020-11-25 22:41:58 +1100

    add FaceRecongnition component with sample image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai'
 
@@ -57,7 +58,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        {/* <FaceRecognition /> */}
+        <FaceRecognition />
       </div>
     );
   }
