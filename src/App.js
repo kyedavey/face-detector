76c219b1c4091cf82cf25118220ec8bcae31221f
Kye Davey <kye.j.davey@gmail.com>
2020-11-25 20:14:43 +1100

    add particles.js https://www.npmjs.com/package/react-particles-js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 120,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' 
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
       <Rank />
