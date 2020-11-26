a8540fdc8f78683d969d75804d6789afe68d2ce5
Kye Davey <kye.j.davey@gmail.com>
2020-11-26 16:41:09 +1100

    pass user state props to Rank, SignIn

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className='white f3'>
-        {'Kye, your current rank is'}
+        {`${name}Kye, your current rank is...`}
       </div>
       <div className='white f1'>
-        {'#5'}
+        {entries}
       </div>
     </div>
   );
