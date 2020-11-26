6b9c7a911cb494dd8a98deb6f158892eac65e3bd
Kye Davey <kye.j.davey@gmail.com>
2020-11-26 22:55:02 +1100

    move clarafai API call to backend, fetch corresponding backend endpoint

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
-const app = new Clarifai.App({
-  apiKey: 'API_KEY'
- });
 
 const particlesOptions = {
   particles: {
@@ -80,10 +77,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl:this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
+      fetch('http://localhost:3000/imageurl', {
+              method: 'post',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                input: this.state.input
+              })
+            })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
